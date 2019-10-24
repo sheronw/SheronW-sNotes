e93f92a3747382837f23c03f654b06edd6de41e9
SheronW <sheronw1174@gmail.com>
2019-10-24 13:03:24 -0500

    Deployed 53564ab with MkDocs version: 1.0.4

--- extra_config.js
+++ extra_config.js
@@ -1,6 +1,12 @@
 // Enable Dollar Sign for inline Latex
-MathJax = {
-  tex: {
-    inlineMath: [['$', '$'], ['\\(', '\\)']]
+MathJax.Hub.Config({
+  tex2jax: {
+    inlineMath: [['$','$'],
+    processEscapes: true
   }
-};
+});
+MathJax.Hub.Config({
+  config: ["MMLorHTML.js"],
+  jax: ["input/TeX", "output/HTML-CSS", "output/NativeMML"],
+  extensions: ["MathMenu.js", "MathZoom.js"]
+});
